d2afd290d2c22a115e52214b0b87848ec2f6b044
ameer0831 <shaikameer4553@gmail.com>
2024-11-06 21:48:47 +0530

    Initial commit

---
+++ project.js
@@ -0,0 +1,17 @@
+var a = "this is text message"
+
+Boolean = true, false
+
+test = 10 > 20;
+caches
+console.log(test);
+
+document.write("this is javascript text message <br><br>")
+document.write(112233)
+document.write("<h1>This is Heading text</h1> <br>")
+
+let sampleView = null;
+console.log(sampleView)
+
+let apple = "red";
+console.log(apple)
